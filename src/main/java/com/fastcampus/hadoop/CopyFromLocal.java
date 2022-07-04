565f8af6895f5305ae89ae6c2d63f73f54da7eb3
kkw855 <kkw855@gmail.com>
2022-07-04 16:12:35 +0900

    WordCount

--- src/main/java/com/fastcampus/hadoop/CopyFromLocal.java
+++ src/main/java/com/fastcampus/hadoop/CopyFromLocal.java
@@ -2,7 +2,6 @@ package com.fastcampus.hadoop;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
